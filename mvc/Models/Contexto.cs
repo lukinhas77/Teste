ecf2867aeda0a7c453453eac7ac8911643886f91
Lucas <lucas.seiki@hotmail.com>
2020-07-16 01:56:00 -0300

    primeiro commit

---
+++ mvc/Models/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace mvc.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() : base(nameOrConnectionString: "StringConexao") { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+    }
+}
---
+++ mvc/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace mvc.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int id { get; set; }
+        [DisplayName("Nome")]
+        public string nome { get; set; }
+        [DisplayName("Senha")]
+        public string senha { get; set; }
+        
+    }
+}
